a753d67107722b225025fcd1c37a528726fcd115
aman-raza <amanraza1234@gmail.com>
2022-01-17 23:30:50 +0530

    5th commit

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -19,31 +19,46 @@ public class SinglyLinkedList {
         SinglyLinkedList sll = new SinglyLinkedList();
 
         // creating nodes
-
 //        head = new ListNode(10);
 //        ListNode second = new ListNode(1);
 //        ListNode third = new ListNode(8);
 //        ListNode fourth = new ListNode(11);
+
+
 //
-//        // connecting all nodes together
-//
+        // connecting all nodes together
 //        head.next = second;
 //        second.next = third;
 //        third.next = fourth;
 
+
         // calling display function
-        display();
-        System.out.println(); // for new line
+//        display();
+//        System.out.println(); // for new line
+
+
 
         // calling & printing value of length function
-        System.out.println("The length of the list is : " + length());
+//        System.out.println("The length of the list is : " + length());
+
+
 
         // calling insertFirst function & displaying it by calling display function
-        insertFirst(1);
-        insertFirst(2);
-        insertFirst(3);
-        insertFirst(4);
-        insertFirst(5);
+//        insertFirst(1);
+//        insertFirst(2);
+//        insertFirst(3);
+//        insertFirst(4);
+//        insertFirst(5);
+//        display();
+
+
+
+        // calling insertFirst function & displaying it by calling display function
+        insertLast(1);
+        insertLast(2);
+        insertLast(3);
+        insertLast(4);
+        insertLast(5);
         display();
 
 
@@ -75,11 +90,26 @@ public class SinglyLinkedList {
         return count;
     }
 
-    // inserting a node
+    // inserting node at the start
 
     public static void insertFirst(int value){
         ListNode newNode = new ListNode(value);
         newNode.next = head;
         head = newNode;
+    }
+
+    // inserting node at the end
+
+    public static  void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        ListNode current = head;
+        while(null != current.next){
+            current = current.next;
+        }
+        current.next = newNode;
     }
 }
