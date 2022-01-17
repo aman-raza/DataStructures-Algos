3208df42eae3f0462114b39e89815e48f3b834d3
aman-raza <amanraza1234@gmail.com>
2022-01-17 22:06:53 +0530

    4th commit

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -20,22 +20,31 @@ public class SinglyLinkedList {
 
         // creating nodes
 
-        head = new ListNode(10);
-        ListNode second = new ListNode(1);
-        ListNode third = new ListNode(8);
-        ListNode fourth = new ListNode(11);
-
-        // connecting all nodes together
-
-        head.next = second;
-        second.next = third;
-        third.next = fourth;
+//        head = new ListNode(10);
+//        ListNode second = new ListNode(1);
+//        ListNode third = new ListNode(8);
+//        ListNode fourth = new ListNode(11);
+//
+//        // connecting all nodes together
+//
+//        head.next = second;
+//        second.next = third;
+//        third.next = fourth;
 
         // calling display function
         display();
         System.out.println(); // for new line
-        // calling & printing length function value
+
+        // calling & printing value of length function
         System.out.println("The length of the list is : " + length());
+
+        // calling insertFirst function & displaying it by calling display function
+        insertFirst(1);
+        insertFirst(2);
+        insertFirst(3);
+        insertFirst(4);
+        insertFirst(5);
+        display();
 
 
     }
@@ -64,5 +73,13 @@ public class SinglyLinkedList {
             current = current.next;
         }
         return count;
+    }
+
+    // inserting a node
+
+    public static void insertFirst(int value){
+        ListNode newNode = new ListNode(value);
+        newNode.next = head;
+        head = newNode;
     }
 }
