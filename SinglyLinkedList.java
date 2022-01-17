3012b3a5538b14be7dac588b1b4d1a2636f33356
aman-raza <amanraza1234@gmail.com>
2022-01-17 12:28:01 +0530

    2nd commit

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -16,6 +16,9 @@ public class SinglyLinkedList {
         sll.head.next = second;
         second.next = third;
         third.next = fourth;
+
+        // calling functions
+        sll.display();
     }
 
     private static class ListNode{
@@ -30,7 +33,14 @@ public class SinglyLinkedList {
 
     private ListNode head;
 
-    public void display(){
+    // display method
 
+    public void display(){
+        ListNode current = head;
+        while(current != null){
+            System.out.print(current.data + "--> ");
+            current = current.next;
+        }
+        System.out.print("null");
     }
 }
