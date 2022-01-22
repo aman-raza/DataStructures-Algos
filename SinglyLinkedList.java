f21b41bd43042e80ecfdda5b6efa96c362364d6d
aman-raza <amanraza1234@gmail.com>
2022-01-22 21:55:40 +0530

    17th commit

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -390,4 +390,23 @@ public class SinglyLinkedList {
         }
         temp.next = current.next;
     }
+
+    // creating a loop in the list
+
+    public static void createALoopInLinkedList(){
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+        ListNode sixth = new ListNode(6);
+
+        head = first;
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = third;
+    }
 }
