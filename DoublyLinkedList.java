61d3940daaab7b1cecd26e65e83829c2a403a015
aman-raza <amanraza1234@gmail.com>
2022-01-25 02:41:59 +0530

    26th commit

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -28,4 +28,26 @@ public class DoublyLinkedList {
     public int length(){
         return length;
     }
+
+    public static void main(String[] args){
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertLast(1);
+        dll.insertLast(10);
+        dll.insertLast(15);
+        dll.insertLast(25);
+    }
+
+    // insert a node at the end
+    public void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if(isEmpty()){
+            head = newNode;
+        }
+        else{
+            tail.next = newNode;
+        }
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
 }
