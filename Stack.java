3a9c807102ba79a5fc6d5d80a1f9466d1b06447f
aman-raza <amanraza1234@gmail.com>
2022-01-29 23:42:40 +0530

    49th commit

--- Stack.java
+++ Stack.java
@@ -1,5 +1,6 @@
 package com.aman.data_structures;
 
+import java.util.Arrays;
 import java.util.EmptyStackException;
 
 public class Stack {
@@ -42,9 +43,15 @@ public class Stack {
 
 
         // calling reverse method & printing the result
-        String str = "ABCD";
-        System.out.println("Before reverse : " + str);
-        System.out.println("After reverse : " + reverse(str));
+//        String str = "ABCD";
+//        System.out.println("Before reverse : " + str);
+//        System.out.println("After reverse : " + reverse(str));
+
+
+        // calling nextGreaterElement method & displaying the result
+        int[] arr = {1, 7, 8, 3, 2, 4, 10};
+        int[] temp = nextGreaterElement(arr);
+        System.out.println(Arrays.toString(temp));
     }
 
 
@@ -92,5 +99,27 @@ public class Stack {
             chars[i] = (char) stack.pop();
         }
         return new String(chars);
+    }
+
+    // find next greater element in the array and store it in the new array
+
+    public static int[] nextGreaterElement(int[] arr){
+        int[] result = new int[arr.length];
+        Stack stack = new Stack();
+        for(int i = arr.length - 1; i >= 0; i--){
+            if(!stack.isEmpty()){
+                while(!stack.isEmpty() && stack.peek() <= arr[i]){
+                    stack.pop();
+                }
+            }
+            if(stack.isEmpty()){
+                result[i] = -1;
+            }
+            else{
+                result[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        return result;
     }
 }
