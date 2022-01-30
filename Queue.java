1da19c7ef58ee4fe0d3ecb6cab275470ded41957
aman-raza <amanraza1234@gmail.com>
2022-01-30 21:21:48 +0530

    51st commit

---
+++ Queue.java
@@ -0,0 +1,30 @@
+package com.aman.data_structures;
+
+public class Queue {
+
+    private ListNode front;
+    private ListNode rear;
+    private int length;
+
+    private class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public int length(){
+        return length;
+    }
+
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
