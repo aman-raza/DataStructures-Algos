aa1f1a892ea5aa5f63a6d5f97110a5e5b0191bc2
aman-raza <amanraza1234@gmail.com>
2022-01-31 22:37:38 +0530

    55th commit

---
+++ BinaryTree.java
@@ -0,0 +1,39 @@
+package com.aman.data_structures;
+
+import javax.swing.tree.TreeNode;
+
+public class BinaryTree {
+
+    private TreeNode root;
+
+    private class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+
+        private int data; // Generic Type
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+
+    public void createBinaryTree(){
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
+
+        root = first; // root ---> first
+
+        first.left = second;
+        first.right = third; // second <--- first ---> third
+
+        second.left = fourth;
+        second.right = fifth; // fourth <--- second ---> fifth
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
