a4f0ea4ea5ae33f9bf1aa66ffde97fbdfba34490
aman-raza <amanraza1234@gmail.com>
2022-02-03 01:56:54 +0530

    67th commit

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -95,4 +95,21 @@ public class BinarySearchTree {
             return search(root.right, key);
         }
     }
+
+    // valid binary search tree
+    public boolean isValid(TreeNode root, long min, long max){
+        if(root == null){
+            return true;
+        }
+        if(root.data <= min || root.data >= max){
+            return false;
+        }
+
+        boolean left = isValid(root.left, min, root.data);
+        if(left){
+            boolean right = isValid(root.right, root.data, max);
+            return false;
+        }
+        return false;
+    }
 }
