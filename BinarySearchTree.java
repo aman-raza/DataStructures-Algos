5b0ffb1922003897ac056311d7616948ba6f77ad
aman-raza <amanraza1234@gmail.com>
2022-02-02 21:01:26 +0530

    65th commit

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -18,5 +18,52 @@ public class BinarySearchTree {
 
     public static void main(String[] args) {
 
+        BinarySearchTree bst = new BinarySearchTree();  // creating an instance of BinarySearchTree
+        bst.insert(6);
+        bst.insert(9);
+        bst.insert(4);
+        bst.insert(2);
+
+
+        // calling inOrder method & printing the result in sorted form
+        bst.inOrder();
+
+    }
+
+
+    // insert a value
+    public void insert(int value){
+        root = insert(root, value);
+    }
+
+    public TreeNode insert(TreeNode root, int value){
+        if(root == null){   // base case
+            root = new TreeNode(value);
+            return root;
+        }
+
+        if(value < root.data){
+            root.left = insert(root.left, value);
+        }
+        else{
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    // creating inOrder tree traversal for printing the tree data
+    // inOrder tree traversal of BinarySearchTree, prints data in sorted form
+
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    public void inOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
     }
 }
