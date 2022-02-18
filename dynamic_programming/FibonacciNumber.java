ec19190afc157074a3d9329d4b744ded124129f2
aman-raza <amanraza1234@gmail.com>
2022-02-19 01:04:06 +0530

    99th commit

--- dynamic_programming/FibonacciNumber.java
+++ dynamic_programming/FibonacciNumber.java
@@ -4,8 +4,17 @@ public class FibonacciNumber {
 
     public static void main(String[] args) {
         FibonacciNumber fn = new FibonacciNumber();
-        System.out.println("The 6th fibonacci number is : " + fn.fib(6));
+
+        // Bottom-Up approach
+//        System.out.println("The 6th fibonacci number is : " + fn.fib(6));
+
+        // Top-Down approach
+        System.out.println("The 6th fibonacci number is : " + fn.fib(new int[6 + 1], 6));
     }
+
+
+
+    // Bottom-Up Approach
 
     public int fib(int n){
         int[] table = new int[n + 1];
@@ -17,5 +26,21 @@ public class FibonacciNumber {
             table[i] = table[i - 1] + table[i - 2];
         }
         return table[n];
+    }
+
+    // Top-Down Approach
+
+    public int fib(int[] memo, int n){
+        if(memo[n] == 0){
+            if(n < 2){
+                memo[n] = n;
+            }
+            else{
+                int left = fib(memo, n - 1);
+                int right = fib(memo, n - 2);
+                memo[n] = left + right;
+            }
+        }
+        return memo[n];
     }
 }
