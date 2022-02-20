6de298d44319bb3b24427798094ac34e38d43b2b
aman-raza <amanraza1234@gmail.com>
2022-02-20 11:41:16 +0530

    102nd commit

--- HashTable.java
+++ HashTable.java
@@ -37,12 +37,35 @@ public class HashTable {
         return size == 0;
     }
 
-    public void put(Integer key, String value){
 
+
+    public void put(Integer key, String value){
+        if(key == null || value == null){
+            throw new IllegalArgumentException("Key or Value is null !!!");
+        }
+
+        int bucketIndex = getBucketIndex(key);
+        HashNode head = buckets[bucketIndex];
+        while(head != null){
+            if(head.key.equals(key)){
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        size++;
+        head = buckets[bucketIndex];
+        HashNode node = new HashNode(key, value);  // (key, value) -> null
+        node.next = head;
+        buckets[bucketIndex] = node;
     }
 
     public String get(Integer key){
         return null;
+    }
+
+    private int getBucketIndex(Integer key){
+        return key % numOfBuckets;
     }
 
     public String remove(Integer key){
@@ -50,6 +73,9 @@ public class HashTable {
     }
 
     public static void main(String[] args) {
-
+        HashTable table = new HashTable(10);
+        table.put(105, "Tom");
+        table.put(21, "Sana");
+        System.out.println(table.size());
     }
 }
