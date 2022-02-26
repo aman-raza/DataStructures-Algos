8c00373c0c696c35f3edf88e2d43dc401d033a65
aman-raza <amanraza1234@gmail.com>
2022-02-26 11:21:43 +0530

    117th commit

---
+++ LongestSubstring.java
@@ -0,0 +1,26 @@
+package com.aman.data_structures;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("alpha"));
+    }
+
+    public static int lengthOfLongestSubstring(String s){
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int start = 0;
+        for(int end = 0; end < s.length(); end++){
+            char rightChar = s.charAt(end);
+            if(map.containsKey(rightChar)){
+                start = Math.max(start, map.get(rightChar) + 1);
+            }
+            map.put(rightChar, end);
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+}
